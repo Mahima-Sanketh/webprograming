2defcf961ace498581f021aec08ceed0535f4424
mallikaarachchi <mahimasanketh12@gamil.com>
2022-07-29 11:42:40 +0530

    4.1.1 version

---
+++ app.js
@@ -0,0 +1,11 @@
+function app() {
+  document.body.style.backgroundColor = 'springgreen';
+}
+
+function mouse() {
+  document.body.style.backgroundColor = 'blue';
+}
+
+function dbclick() {
+  document.body.style.backgroundColor = 'pink';
+}
