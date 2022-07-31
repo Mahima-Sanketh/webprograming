554460d463b03bd26a5301269e0aa30ed0e04eb9
mallikaarachchi <mahimasanketh12@gamil.com>
2022-07-31 10:57:52 +0530

    4.1.2 version

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ function mouse() {
 function dbclick() {
   document.body.style.backgroundColor = 'pink';
 }
+
+//function myFunction() {
+//var table = document.getElementById('mytable');
+//var row = table.insertRow(table.length);
+
+//var x = document.getElementById('typetext').value;
+//var y = document.getElementById('typeid').value;
+//var r = document.getElementById('typenumber').value;
+
+//var cell1 = row.insertCell(0);
+//var cell2 = row.insertCell(1);
+//var cell3 = row.insertCell(2);
+
+//cell1.innerHtml = x;
+//cell2.innerHtml = y;
+//cell3.innerHtml = r;
+//}
